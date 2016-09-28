1793770c7ba388b5d510d8da789008b7414fd467
PreethiECE <preethiece96@gmail.com>
2016-09-28 15:05:09 +0530

    Create simple.java

---
+++ simple.java
@@ -0,0 +1,11 @@
+public static void main(String[] arg)
+{
+int n;
+float p,r,si;
+Scanner in=new Scanner(System.in);
+n=in.nextInt();
+p=in.nextFloat();
+r=in.nextFloat();
+si=(p*n*r)/100;
+System.out.println(si);
+}
